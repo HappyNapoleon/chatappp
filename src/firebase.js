0d46cc4b2c9f2aba3ab5a5865a3699ff9f5e0896
Ranvir Singh <123026576+HappyNapoleon@users.noreply.github.com>
2024-02-17 16:54:40 -0500

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDogdfZo8_QFkj7LofeQP42S-r9CEci9_c",
-  authDomain: "chatapp-3d1db.firebaseapp.com",
-  projectId: "chatapp-3d1db",
-  storageBucket: "chatapp-3d1db.appspot.com",
-  messagingSenderId: "283420372762",
-  appId: "1:283420372762:web:18ca69eecff108bdd0c6a3",
-  measurementId: "G-HC6JW1XSMW"
+    apiKey: "AIzaSyDogdfZo8_QFkj7LofeQP42S-r9CEci9_c",
+    authDomain: "chatapp-3d1db.firebaseapp.com",
+    projectId: "chatapp-3d1db",
+    storageBucket: "chatapp-3d1db.appspot.com",
+    messagingSenderId: "283420372762",
+    appId: "1:283420372762:web:18ca69eecff108bdd0c6a3",
+    measurementId: "G-HC6JW1XSMW"
 };
 
 
